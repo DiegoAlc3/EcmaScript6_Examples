f904d54506caa587d9399e1f82a62902cc95a9ec
Diego Alcantara <diegosalc15@gmail.com>
2019-12-12 07:58:30 -0300

    Using template literals of the ES6

---
+++ sixth_example/bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// USING TEMPLATE LITERALS
+var usuario = 'Diego';
+var idade = 23; // console.log('O usuário ' + usuario + ' possui ' + idade + ' anos'); WITHOUT TEMPLATE LITERALS
+
+console.log("O usuario ".concat(usuario, " possui ").concat(idade, " anos")); // WITH TEMPLATE LITERALS
---
+++ sixth_example/main.js
@@ -0,0 +1,6 @@
+// USING TEMPLATE LITERALS
+
+const usuario = 'Diego';
+const idade = 23;
+// console.log('O usuário ' + usuario + ' possui ' + idade + ' anos'); WITHOUT TEMPLATE LITERALS
+console.log(`O usuario ${usuario} possui ${idade} anos`); // WITH TEMPLATE LITERALS
