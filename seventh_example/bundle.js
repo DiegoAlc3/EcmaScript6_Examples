645323fd6e4a17c915c5a823da3663a30e40d4ba
Diego Alcantara <diegosalc15@gmail.com>
2019-12-12 08:03:17 -0300

    Using Object Short Syntax of the ES6

--- seventh_example/bundle.js
+++ seventh_example/bundle.js
@@ -1,42 +1,16 @@
 "use strict";
 
-// USING OPERATOR MAP
-var usuarios = [{
-  nome: 'Diego',
-  idade: 23,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Gabriel',
-  idade: 15,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Lucas',
-  idade: 30,
-  empresa: 'Facebook'
-}];
-var idades = usuarios.map(function (usuarios) {
-  return usuarios.idade;
-});
-console.log("MAP EXAMPLE");
-console.log(idades); // USING FILTER
+// USING OBJECT SHORT Syntax
+var nome = 'Diego';
+var idade = 23; // const usuario = {  WITHOUT OBJECT SHORT Syntax
+//  nome: nome,
+//  idade: idade,
+//  cidade: 'Rio do Sul',
+// };
 
-var filter = usuarios.filter(function (usuarios) {
-  return usuarios.empresa == "Rocketseat" && usuarios.idade >= 18;
-});
-console.log("FILTER EXAMPLE");
-console.log(filter); // USING FIND
-
-var find = usuarios.find(function (usuarios) {
-  return usuarios.empresa === "Google";
-});
-console.log("FIND EXAMPLE"); // RESPONSE (UNDEFINED) CAUSE DON'T HAVE (EMPRESA) = "Google"
-
-console.log(find);
-var multiply = usuarios.map(function (usuarios) {
-  usuarios.idade *= 2;
-  return usuarios;
-});
-var filtering = multiply.filter(function (multiply) {
-  return multiply.idade <= 50;
-});
-console.log(filtering);
+var usuario = {
+  // WITH OBJECT SHORT Syntax
+  nome: nome,
+  idade: idade,
+  cidade: 'Rio do Sul'
+};
