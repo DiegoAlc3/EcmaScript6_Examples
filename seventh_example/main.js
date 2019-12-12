5d4ba8b3df7a29c81b9006f5574ebabb1351aae9
Diego Alcantara <diegosalc15@gmail.com>
2019-12-12 08:02:17 -0300

    Using Object Short Syntax of the ES6

---
+++ seventh_example/main.js
@@ -0,0 +1,16 @@
+// USING OBJECT SHORT Syntax
+
+const nome = 'Diego';
+const idade = 23;
+
+// const usuario = {  WITHOUT OBJECT SHORT Syntax
+//  nome: nome,
+//  idade: idade,
+//  cidade: 'Rio do Sul',
+// };
+
+const usuario = { // WITH OBJECT SHORT Syntax
+ nome,
+ idade,
+ cidade: 'Rio do Sul',
+};
