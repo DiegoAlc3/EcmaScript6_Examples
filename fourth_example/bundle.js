148251c90199a2a20cc4d7ad55955525fbd2b666
Diego Alcantara <diegosalc15@gmail.com>
2019-12-12 07:31:42 -0300

    Using Restructuring in functions

---
+++ fourth_example/bundle.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// Restructuring
+var empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC'
+  }
+};
+var nome = empresa.nome,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nome); // Rocketseat
+
+console.log(cidade); // Rio do Sul
+
+console.log(estado); // SC
+// Restructuring in function
+
+function mostraInfo(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
+}
+
+mostraInfo({
+  nome: 'Diego',
+  idade: 23
+});
---
+++ fourth_example/main.js
@@ -0,0 +1,23 @@
+// Restructuring
+const empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+  cidade: 'Rio do Sul',
+  estado: 'SC',
+  }
+ };
+
+const {nome, endereco:{cidade,estado}} = empresa;
+console.log(nome); // Rocketseat
+console.log(cidade); // Rio do Sul
+console.log(estado); // SC
+
+// Restructuring in function
+
+function mostraInfo({nome,idade}) {
+  return `${nome} tem ${idade} anos.`;
+ }
+mostraInfo({ nome: 'Diego', idade: 23 });
+ 
+
+ 
